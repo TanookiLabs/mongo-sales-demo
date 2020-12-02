bb1ccc94c7f3b92aee613b82fd3e812d39c83310
Mark Reist <evendir@gmail.com>
2020-12-02 14:01:34 -0500

    Setup Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ Bundler.require(*Rails.groups)
 
 module MongoSalesdemo
   class Application < Rails::Application
+    config.active_job.queue_adapter = :sidekiq
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
